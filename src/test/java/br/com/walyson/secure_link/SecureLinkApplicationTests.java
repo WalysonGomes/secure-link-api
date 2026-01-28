6b96c5e765af62440bc94ef253bab4173ace5566
WalysonGomes <gomeswalyson258@gmail.com>
2026-01-28 16:45:41 -0300

    initial commit

---
+++ src/test/java/br/com/walyson/secure_link/SecureLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walyson.secure_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
